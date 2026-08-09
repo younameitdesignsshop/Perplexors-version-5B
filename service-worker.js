acaa8c601c4880f49fbcc54c97af46bad5cdae62
younameitdesignsshop <younameitdesignsshop@gmail.com>
2026-08-09 18:50:09 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,42 @@
-const C='deduction-columns-v11b';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon.svg']))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='deduction-columns-v11b-cachefix-20260809-1843';
+const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.mode==='navigate'){
+    event.respondWith(
+      fetch(request)
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put('index.html',copy));
+          return response;
+        })
+        .catch(()=>caches.match('index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(request)
+      .then(response=>{
+        if(request.method==='GET'){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(request,copy));
+        }
+        return response;
+      })
+      .catch(()=>caches.match(request))
+  );
+});
