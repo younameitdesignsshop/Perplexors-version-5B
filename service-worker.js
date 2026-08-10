75c2549c5f9655cdbf838894a53dc7c7b976fed7
younameitdesignsshop <younameitdesignsshop@gmail.com>
2026-08-10 15:43:17 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='deduction-columns-v13-20260810';
+const CACHE='deduction-columns-v13a-layoutfit-20260810';
 const CORE=['./','index.html','manifest.webmanifest','icon.svg'];
 
 self.addEventListener('install',event=>{
